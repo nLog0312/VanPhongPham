0c9e4543847bf15cba371e478d80a46333d81fbb
Nguyễn Tuấn Thành <ngtuanthanh.official@gmail.com>
2023-10-27 21:17:05 +0700

    Giao diện

---
+++ Main/Register.php
@@ -0,0 +1,43 @@
+<style>
+    <?php include 'CSS/Register.css' ?>
+</style>
+<div>
+    <div class="register-war1">
+        <div class="logo-icon">
+            <div class="size-logo-icon">
+                <img class="image-logo-login" src="picture/logoHH-do.png">
+            </div>
+            <div class="x0104"></div>
+            <div class="x0103">Đăng ký</div>
+        </div>
+    </div >
+        <div class="bg-img-size2">
+            <img class="register-pic" src="picture/bg-login@2x.png"/>
+    <div class="container3">
+        <div class="registration form">
+            <header>Register</header>
+                <form action="#">
+                    <input type="text" placeholder="Enter your email">
+                    <input type="password" placeholder="Create a password">
+                    <input type="password" placeholder="Confirm your password">
+                    <input type="button" class="button" value="Signup" onclick="chuyenHuong()">
+                </form>
+            <div class="signup">
+                    <span class="signup">Đã có tài khoản?
+                        <label onclick="chuyenHuong2()">Đăng nhập</label>
+                     </span>
+            </div>
+        </div>
+    </div>
+    </div>
+
+</div>
+
+<script type="text/javascript">
+    function chuyenHuong() {
+        window.location.href = 'index.php';
+    }
+    function chuyenHuong2() {
+        window.location.href = 'Login.php';
+    }
+</script>
---
+++ Main/body.php
@@ -0,0 +1,157 @@
+<style>
+    <?php include 'CSS/body.css' ?>
+</style>
+<div class="section-banner-hotword--no-skin">
+    <div class="event_shop">
+        <div class="container">
+            <div class="full-home">
+                <div class="main-banner">
+                    <div class="slider">
+                        <div class="btn-prev">
+                            <div class="prev-icon"><</div>
+                        </div>
+                        <div class="btn-next">
+                            <div class="next-icon">></div>
+                        </div>
+                        <div class="slides">
+                            <input type="radio" name="radio-btn" id="radio1">
+                            <input type="radio" name="radio-btn" id="radio2">
+                            <input type="radio" name="radio-btn" id="radio3">
+                            <input type="radio" name="radio-btn" id="radio4">
+                            <input type="radio" name="radio-btn" id="radio5">
+
+                            <div class="slide first">
+                                <img src="picture/1_6.jpg" alt="" class="size-img">
+                            </div>
+                            <div class="slide">
+                                <img src="picture/1_2.jpg" alt="">
+                            </div>
+                            <div class="slide">
+                                <img src="picture/1_3.jpg" alt="">
+                            </div>
+                            <div class="slide">
+                                <img src="picture/1_4.png" alt="">
+                            </div>
+                            <div class="slide">
+                                <img src="picture/1_5.jpg" alt="">
+                            </div>
+                            <div class="navigation-auto">
+                                <div class="auto-btn-1"></div>
+                                <div class="auto-btn-2"></div>
+                                <div class="auto-btn-3"></div>
+                                <div class="auto-btn-4"></div>
+                                <div class="auto-btn-5"></div>
+                            </div>
+
+                        </div>
+
+                        <div class="navigation-mannual">
+                            <label for="radio1" class="mannual-btn"></label>
+                            <label for="radio2" class="mannual-btn"></label>
+                            <label for="radio3" class="mannual-btn"></label>
+                            <label for="radio4" class="mannual-btn"></label>
+                            <label for="radio5" class="mannual-btn"></label>
+                        </div>
+
+
+                    </div>
+                    <script>
+                        <?php include 'slide.js' ?>
+                    </script>
+
+
+                </div>
+                <div class="extra-right-banner">
+                    <a href="#banner2">
+                        <img class="war2" src="picture/1_5.jpg"/>
+                    </a>
+                    <a href="#banner3">
+                        <img class="war3" src="picture/1_2.jpg"/>
+                    </a>
+                </div>
+            </div>
+
+            <div class="_icon">
+                <a class="_link-icon" href="#freeship">
+                    <div class="_top-icon" style="width: 150px">
+                        <div class="_ss1 _bo1">
+                            <img class="_size-icon" src="picture/freeship.png"/>
+                        </div>
+                        <div class="_size-icon-text">
+                            Miễn phí ship - giao hàng tận nơi
+                        </div>
+                    </div>
+                </a>
+                <a class="_link-icon" href="#freeship">
+                    <div class="_top-icon" style="width: 150px">
+                        <div class="_ss1 _bo1">
+                            <img class="_size-icon" src="picture/voucher.png"/>
+                        </div>
+                        <div class="_size-icon-text">
+                            Săn voucher - <br/> Giảm sập sàn
+                        </div>
+                    </div>
+                </a>
+                <a class="_link-icon" href="#freeship">
+                    <div class="_top-icon" style="width: 150px">
+                        <div class="_ss1 _bo1">
+                            <img class="_size-icon" src="picture/50ps.png"/>
+                        </div>
+                        <div class="_size-icon-text">
+                            Giảm giá lên <br/> đến 50%
+                        </div>
+                    </div>
+                </a>
+                <a class="_link-icon" href="#freeship">
+                    <div class="_top-icon" style="width: 150px">
+                        <div class="_ss1 _bo1">
+                            <img class="_size-icon" src="picture/qt.png"/>
+                        </div>
+                        <div class="_size-icon-text">
+                           Săn hàng quốc tế
+                        </div>
+                    </div>
+                </a>
+                <a class="_link-icon" href="#freeship">
+                    <div class="_top-icon" style="width: 150px">
+                        <div class="_ss1 _bo1">
+                            <img class="_size-icon" src="picture/fire.png"/>
+                        </div>
+                        <div class="_size-icon-text">
+                            Săn hàng giá rẻ
+                        </div>
+                    </div>
+                </a>
+                <a class="_link-icon" href="#freeship">
+                    <div class="_top-icon" style="width: 150px">
+                        <div class="_ss1 _bo1">
+                            <img class="_size-icon" src="picture/kho.png"/>
+                        </div>
+                        <div class="_size-icon-text">
+                           Xả kho
+                        </div>
+                    </div>
+                </a>
+                <a class="_link-icon" href="#freeship">
+                    <div class="_top-icon" style="width: 150px">
+                        <div class="_ss1 _bo1">
+                            <img class="_size-icon" src="picture/tonghop.png"/>
+                        </div>
+                        <div class="_size-icon-text">
+                            Văn phòng phẩm <br/> tổng hợp
+                        </div>
+                    </div>
+                </a>
+            </div>
+    </div>
+
+        <?php
+        include('danhmuc.php');
+        include('goiy.php');
+        include('dssanpham.php');
+        include('Footer.php');
+        ?>
+         </div>
+     </div>
+    </div>
+</div>
